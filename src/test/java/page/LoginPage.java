ac8fc5366d5454341e5658f3ab03ea477567146e
AksharaBhanu <javagalbhanu@gmail.com>
2025-01-09 06:23:20 +0530

    Added Err Msg to LoginPage

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
 public class LoginPage {
 	@FindBy(id="input-username")
@@ -14,6 +17,9 @@ public class LoginPage {
 	
 	@FindBy(name="login-button")
 	private WebElement goButton;
+	
+	@FindBy(css="div.error")
+	private WebElement errMsg;
 	
 	public LoginPage(WebDriver driver)
 	{
@@ -33,5 +39,21 @@ public class LoginPage {
 	public void clickGoButton()
 	{
 		goButton.click();
+	}
+	
+	public boolean verifyErrMsgIsDisplayed(WebDriverWait wait)
+	{
+		try
+		{
+			wait.until(ExpectedConditions.visibilityOf(errMsg));
+			Reporter.log("Err Msg is Displayed",true);
+			return true;
+		}
+		catch (Exception e) 
+		{
+			Reporter.log("Err Msg is NOT Displayed",true);
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
